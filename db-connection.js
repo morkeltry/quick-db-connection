871faf339e55695affdcfc76f3a233d48e6967fb
Shinies <github@morkeltry.33mail.com>
2019-11-08 11:51:22 +0000

    Added db connection code

---
+++ db-connection.js
@@ -0,0 +1,52 @@
+var mysql = require('mysql');//import mysql library
+var externalip = require('externalip');// see below
+
+externalip(function (err, ip) {
+  console.log(ip); // => 8.8.8.8
+}); // It might be that the IP address of the Replit is blocked, if this is the case then they should get the IP and we can whitelist it.
+
+var con = mysql.createConnection({
+  host: "35.189.121.127",
+  user: "D0002",
+  password: "password569206",
+  database: "cards"
+});
+// the connection data
+
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+//comment out below to get all cards
+//  let x = parseInt(Math.ceil(Math.random()*52))
+//  let sql = 'select pip,suit from cards where id ='+x;
+//uncomment below to get all cards
+ let sql = 'select pip, suit from cards'
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    if (result) {processData(result)};
+  });
+
+});
+
+function processData(data) {
+  let i = 0;
+  let cards = [];
+  for (row of data) { //loops through data
+   cards.push(row.pip+row.suit) //pushes cards to cards array
+    // for (key of Object.keys(row)) {
+    //   console.log(key+ " : "+row[key])
+    // }
+  i++;
+  }
+  callMe(cards) // calls function with cards array
+}
+
+
+ function callMe(cards) {
+   //uncomment out this to get all cards
+    console.log(cards)
+   //comment out the below for all cards
+   let i=0
+   //console.log(cards[i])
+ }

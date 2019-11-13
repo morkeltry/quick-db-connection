83430cec27b73e6ea2980bb1934b3405fec8dcb8
Shinies <github@morkeltry.33mail.com>
2019-11-13 13:05:50 +0000

    sends a fetch request on form submit (but still need to fix CORS)

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 // const exphbs = require ('express-handlebars');
 
@@ -10,5 +11,8 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.use(router);
+app.use(cors({
+  origin: '*'
+}));
 
 module.exports = app;
